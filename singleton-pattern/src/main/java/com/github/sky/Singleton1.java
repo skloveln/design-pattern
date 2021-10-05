c17e5b0f86487b1a7ec854217da1557cf045cc54
skloveln <490763125@qq.com>
2021-10-05 16:41:55 +0800

    code clean

--- singleton-pattern/src/main/java/com/github/sky/Singleton1.java
+++ singleton-pattern/src/main/java/com/github/sky/Singleton1.java
@@ -2,15 +2,17 @@ package com.github.sky;
 
 /**
  * 饿汉式
+ *
+ * @author sukai
  */
 public class Singleton1 {
 
-    private final static Singleton1 singleton = new Singleton1();
+    private final static Singleton1 SINGLETON = new Singleton1();
 
     private Singleton1(){ }
 
     public static Singleton1 getInstance(){
-        return singleton;
+        return SINGLETON;
     }
 
     public static void doSomething(String[] args) {
